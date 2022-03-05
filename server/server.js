6d07a35c9db60d81d0ed12a37a919903bfe49008
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-03-04 18:09:57 -1000

    h10 debug

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
+const app = express();
 
 const PORT = process.env.PORT || 5000  
 
