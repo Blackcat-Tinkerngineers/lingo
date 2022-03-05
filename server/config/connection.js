30370e5cf458b1ec5df8ca2794f707e78b8a58ca
Chelsie Leilani Bangloy-Gonsalves <88634637+Blackcat-Tinkerngineers@users.noreply.github.com>
2022-03-05 11:09:00 -1000

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 require('dotenv').config({ path: 'ENV_FILENAME' });
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://wordlingoers:t0OpX5WYYXMiutuV@cluster0.1ckxt.mongodb.net/wordlingodb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
