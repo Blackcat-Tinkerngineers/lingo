ddfee2febb5c15155b506710f5f57eaf1a92b38f
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-03-04 21:53:56 -1000

    commit update

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,13 +3,13 @@ const dotenv = require("dotenv");
 require("dotenv").config();
 
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, 
+mongoose.createConnection(uri, 
   {
     useNewUrlParser:true, 
     useCreateIndex:true
   });
 
-const connection = mongoose.createConnection();
+let connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
