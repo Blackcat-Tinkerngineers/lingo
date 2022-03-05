5989fe00620f7b082732944073a591730f55d8e0
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-03-05 10:45:09 -1000

    server update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-require("dotenv").config();
+require('dotenv').config({ path: 'ENV_FILENAME' });
 
-const uri = process.env.MONGODB_URI;
-mongoose.createConnection(uri, 
-  {
-    useNewUrlParser:true, 
-    useCreateIndex:true
-  });
-
-let connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://wordlingoers:t0OpX5WYYXMiutuV@cluster0.1ckxt.mongodb.net/wordlingodb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
 module.exports = mongoose.connection;
