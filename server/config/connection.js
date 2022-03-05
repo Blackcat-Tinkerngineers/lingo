53f1da8cd11bb62cafc515e80e8e72063925d85f
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-03-04 17:44:29 -1000

    commit update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/wordlingo",
+  process.env.MONGODB_URI || "mongodb+srv://wordlingoers:66Py2E1rurPhn1VZ@cluster0.1ckxt.mongodb.net/wordlingoDB?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
