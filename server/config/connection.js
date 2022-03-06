87603b8a96105c627d07d5b5a9d76ec5bc9c02a4
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-03-05 19:23:06 -1000

    update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-require('dotenv').config()
-console.log(process.env) 
-const mongoose = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const uri = "";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-=======
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-const mySecret = process.env['cluster0']
+const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/wordlingo",
@@ -24,10 +7,5 @@ mongoose.connect(
     useUnifiedTopology: true,
   }
 );
->>>>>>> c6244dfb8588a18a28978727c2a494d079f7b1f0
 
 module.exports = mongoose.connection;
-
-
-
-
